1e12fef525a93f9e2b95c3ccdac0694183fdf146
Ole <ole.keil@gmail.com>
2023-06-07 15:42:47 +0200

    Q4.a and b, add configs for questions

--- src/models/cnn/vgg11_bn.py
+++ src/models/cnn/vgg11_bn.py
@@ -12,7 +12,11 @@ class VGG11_bn(BaseModel):
         # TODO: Initialize the different model parameters from the config file  #
         # for activation and norm_layer refer to cnn/model.py
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-        
+        self._layer_config = layer_config
+        self._num_classes = num_classes
+        self._activation = getattr(nn, activation["type"])(**activation["args"])
+        self._norm_layer = norm_layer
+        self._fine_tune = fine_tune
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         self._pretrained = pretrained
         self._build_model()
@@ -29,8 +33,26 @@ class VGG11_bn(BaseModel):
         # the fine_tune flag.                                                           #
         #################################################################################
         self.vgg11_bn = torchvision.models.vgg11_bn(pretrained=self._pretrained)
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****        
-        
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        self.hidden1 = nn.Linear(25088, self._layer_config[0])
+        self.hidden2 = nn.Linear(self._layer_config[0], self._layer_config[1])
+        self.output = nn.Linear(self._layer_config[1], self._num_classes)
+
+        self.vgg11_bn.classifier = nn.Sequential(
+            self.hidden1,
+            self._activation,
+            getattr(nn, self._norm_layer["type"])(self._layer_config[0]),
+            self.hidden2,
+            self._activation,
+            getattr(nn, self._norm_layer["type"])(self._layer_config[1]),
+            self.output
+        )
+
+        if not self._fine_tune:
+            for param in self.vgg11_bn.parameters():
+                param.requires_grad = False
+            for param in self.vgg11_bn.classifier.parameters():
+                param.requires_grad = True        
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     def forward(self, x):
@@ -38,6 +60,6 @@ class VGG11_bn(BaseModel):
         # TODO: Implement the forward pass computations                                 #
         #################################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-        
+        x = self.vgg11_bn(x)
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return x
